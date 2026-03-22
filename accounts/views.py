2d0af35b66f6a97b46498980561134758b25ea27
Duncan <d.devengine@gmail.com>
2026-03-22 17:45:48 +0100

    feat(accounts): sign in, log out and navbar (US-202)

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
 
 # Create your views here.
@@ -14,3 +15,18 @@ def signup(request):
         form = SignUpForm()
         
     return render(request, "accounts/signup.html", {"form": form})
+
+def signin(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("vehicles:vehicle_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("vehicles:vehicle_list")
