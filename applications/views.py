e164057f3dd98cb07f08f268c164c902b247ad7e
Duncan <d.devengine@gmail.com>
2026-04-12 18:24:13 +0200

    fix: remove duplicate application creation, close navbar link

--- applications/views.py
+++ applications/views.py
@@ -6,20 +6,13 @@ from .models import Application, Document
 @login_required
 def apply_purchase(request, vehicle_pk):
     vehicle = get_object_or_404(Vehicle, pk=vehicle_pk, is_active=True)
-
     if request.method == "POST":
-        Application.objects.create(
-            applicant=request.user,
-            vehicle=vehicle,
-            application_type=Application.PURCHASE,
-        )
         application = Application.objects.create(
             applicant=request.user,
             vehicle=vehicle,
             application_type=Application.PURCHASE,
         )
         return redirect("applications:upload_documents", pk=application.pk)
-    
     return render(request, "applications/apply_purchase.html", {"vehicle": vehicle})
 
 @login_required
